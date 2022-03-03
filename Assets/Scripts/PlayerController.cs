9fb1c2d8739116b04a2693f3014e8428b7b57b04
Arisa Alcantara <siararisa@gmail.com>
2022-03-03 10:35:32 +0800

    Added player movement and tilemaps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField]
+    private float moveSpeed = 2.0f;
+    [SerializeField]
+    private float jumpForce = 250f;
+    
+    [Header("Ground")]
+    [SerializeField]
+    //Where is the feet of our player?
+    private Transform groundPoint;
+    [SerializeField]
+    //How big is the radius of our ground checker?
+    private float groundRadius = 0.2f;
+    [SerializeField]
+    //What objects are considered as ground?
+    private LayerMask groundLayers;
+
+    private Rigidbody2D playerRigidBody;
+    private SpriteRenderer spriteRenderer;
+
+    private float horizontalInput;
+    private bool facingRight = true;
+    private bool isJump = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Get the movement input from the user
+        horizontalInput = Input.GetAxis("Horizontal");
+        Flip(horizontalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            isJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //move the player based on the physics update
+        float horizontalMovement = horizontalInput * moveSpeed;
+        playerRigidBody.velocity = new Vector2(horizontalMovement, playerRigidBody.velocity.y);
+
+        Debug.Log(IsGrounded());
+
+        if (isJump && IsGrounded())
+        {
+            playerRigidBody.AddForce(new Vector2(0, jumpForce));
+            isJump = false;
+        }
+    }
+
+  
+    private void Flip(float movement)
+    {
+        //if we have positive input and not facing right (facing left)
+        // OR
+        //if we have negative input and we are facing right
+        if(movement > 0 && !facingRight || movement < 0 && facingRight)
+        {
+            //toggle the boolean
+            facingRight = !facingRight;
+            spriteRenderer.flipX = !facingRight;
+        }
+    }
+
+    /// <summary>
+    /// This function will return true if our feet is touching the ground and false if not
+    /// </summary>
+    /// <returns></returns>
+    private bool IsGrounded()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundPoint.position, groundRadius, groundLayers);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)//make sure that the gameobject we are colliding is not our own
+                return true;
+        }
+        return false;
     }
 }
